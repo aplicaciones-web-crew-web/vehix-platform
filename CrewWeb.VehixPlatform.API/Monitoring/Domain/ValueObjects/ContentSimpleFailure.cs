8dadb0a09b134260adbfb5efa6db55592f1c52cf
sergioJM05 <sergiogino8@hotmail.com>
2025-06-17 09:46:39 -0500

    feat(monitoring): add monitoring value objects.

---
+++ CrewWeb.VehixPlatform.API/Monitoring/Domain/ValueObjects/ContentSimpleFailure.cs
@@ -0,0 +1,3 @@
+namespace CrewWeb.VehixPlatform.API.Monitoring.Domain.ValueObjects;
+
+public record ContentSimpleFailure(string FailureTitle);
---
+++ CrewWeb.VehixPlatform.API/Monitoring/Domain/ValueObjects/ContentTechnicalFailure.cs
@@ -0,0 +1,3 @@
+namespace CrewWeb.VehixPlatform.API.Monitoring.Domain.ValueObjects;
+
+public record ContentTechnicalFailure(string ErrorCode, string FailureTitle, string SuggestSolution);
