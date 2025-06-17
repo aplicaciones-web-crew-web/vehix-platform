e7bb159afa22db7937a06bd25207a7d76b39032d
Dayro Rios <dayro.rios.p@gmail.com>
2025-06-17 18:39:25 -0500

    feat(subscriptions): add value objects and entities

---
+++ CrewWeb.VehixPlatform.API/Subscriptions-and-payments/Domain/Model/Entities/Subscription.cs
@@ -0,0 +1,26 @@
+namespace CrewWeb.VehixPlatform.API.Subscriptions_and_payments.Domain.Model.Entities;
+using CrewWeb.VehixPlatform.API.Subscriptions_and_payments.Domain.Model.ValueObjects;
+
+public class Subscription
+{
+    public int Id { get; set; }
+    
+    public SubscriptionType Type { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public bool IsActive { get; set; }
+
+    public Subscription(int id, DateTime startDate, SubscriptionType type, DateTime endDate, bool isActive)
+    {
+        Id = id;
+        Type = type;
+        StartDate = startDate;
+        EndDate = endDate;
+        IsActive = isActive;
+    }
+    
+    
+    
+
+    // Additional properties or methods can be added as needed
+}
