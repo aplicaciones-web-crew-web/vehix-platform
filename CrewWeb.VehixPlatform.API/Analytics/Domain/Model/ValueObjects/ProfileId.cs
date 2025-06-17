6e454473308385818c4b40643da1928ecb0c0911
sergioJM05 <sergiogino8@hotmail.com>
2025-06-17 09:25:15 -0500

    feat(analytics): delete profile id, because there is no relation with the context.

--- CrewWeb.VehixPlatform.API/Analytics/Domain/Model/ValueObjects/ProfileId.cs
+++
@@ -1,8 +0,0 @@
-namespace CrewWeb.VehixPlatform.API.Analytics.Domain.Model.ValueObjects;
-/// <summary>
-/// Represents a unique identifier for a profile.
-/// </summary>
-/// <param name="Id">
-/// The unique identifier for the profile.
-/// </param>
-public record ProfileId(int Id);
