908c37791a8f9ddda7858225dce5fc23878f630d
Dayro Rios <dayro.rios.p@gmail.com>
2025-06-17 18:41:23 -0500

    feat(subscriptions): add money value object

---
+++ CrewWeb.VehixPlatform.API/Subscriptions-and-payments/Domain/Model/ValueObjects/Money.cs
@@ -0,0 +1,7 @@
+namespace CrewWeb.VehixPlatform.API.Subscriptions_and_payments.Domain.Model.ValueObjects;
+
+public class Money
+{
+    public decimal Amount { get; set; }
+    public string Currency { get; set; }
+}
