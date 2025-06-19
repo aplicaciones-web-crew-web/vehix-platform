7d055b4c0c20fa5f90fd6a37b02fca01eea26851
sergioJM05 <sergiogino8@hotmail.com>
2025-06-19 18:00:25 -0500

    fix(project): add cortex.mediator dependencies.

--- CrewWeb.VehixPlatform.API/Monitoring/Domain/Model/Events/BadPracticeCreatedEvent.cs
+++ CrewWeb.VehixPlatform.API/Monitoring/Domain/Model/Events/BadPracticeCreatedEvent.cs
@@ -1,6 +1,8 @@
+using ACME.LearningCenterPlatform.API.Shared.Domain.Model.Events;
+
 namespace CrewWeb.VehixPlatform.API.Monitoring.Domain.Model.Events;
 
-public class BadPracticeCreatedEvent
+public class BadPracticeCreatedEvent(string descriptionBadPractice) : IEvent
 {
     
 }
