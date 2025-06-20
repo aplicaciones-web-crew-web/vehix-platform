57dd876d5971917ad5330eca37e85e5515e379cb
AntonioNavarro24 <137426429+AntonioNavarro24@users.noreply.github.com>
2025-06-20 01:39:49 -0500

    feat(management): add management entity.

---
+++ CrewWeb.VehixPlatform.API/Management/Domain/Model/Entities/LibraryItem.cs
@@ -0,0 +1,26 @@
+﻿using CrewWeb.VehixPlatform.API.Management.Domain.Model.ValueObjects;
+
+namespace CrewWeb.VehixPlatform.API.Management.Domain.Model.Entities;
+
+public class LibraryItem
+{
+    public int Id { get; }
+    public ELibraryType Type { get; private set; }
+    public string Title { get; private set; }
+    public string Description { get; private set; }
+
+    protected LibraryItem()
+    {
+    }
+
+    public LibraryItem(ELibraryType type, string title, string description)
+    {
+        if (string.IsNullOrWhiteSpace(title)) throw new ArgumentException("Title cannot be empty.", nameof(title));
+        if (string.IsNullOrWhiteSpace(description)) throw new ArgumentException("Description cannot be empty.", nameof(description));
+
+        Type = type; //
+        Title = title;
+        Description = description;
+    }
+
+}
