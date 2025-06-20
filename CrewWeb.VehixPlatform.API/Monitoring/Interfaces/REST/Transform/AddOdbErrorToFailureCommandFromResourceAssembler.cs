51296db8cbc3594b9517a2efc0cf045d26709192
sergioJM05 <sergiogino8@hotmail.com>
2025-06-20 16:02:28 -0500

    feature(monitoring): add monitoring transform interfaces.

---
+++ CrewWeb.VehixPlatform.API/Monitoring/Interfaces/REST/Transform/AddOdbErrorToFailureCommandFromResourceAssembler.cs
@@ -0,0 +1,13 @@
+using CrewWeb.VehixPlatform.API.Monitoring.Domain.Model.Commands;
+using CrewWeb.VehixPlatform.API.Monitoring.Interfaces.REST.Resources;
+
+namespace CrewWeb.VehixPlatform.API.Monitoring.Interfaces.REST.Transform;
+
+public static class AddOdbErrorToFailureCommandFromResourceAssembler
+{
+    public static AddOdbErrorToFailureCommand ToCommandFromResource(AddOdbErrorToFailureResource resource,
+        int failureId)
+    {
+        return new AddOdbErrorToFailureCommand(failureId);
+    }
+}
