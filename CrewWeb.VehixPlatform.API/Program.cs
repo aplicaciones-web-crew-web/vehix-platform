0022185c981b61e3d62990b476f33233d275351b
Piero Elescano <pieroelescano300@gmail.com>
2025-06-22 01:20:40 -0500

    feat: update Program.cs

--- CrewWeb.VehixPlatform.API/Program.cs
+++ CrewWeb.VehixPlatform.API/Program.cs
@@ -133,11 +133,14 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Use Swagger for API documentation if in development mode
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+ //   app.UseSwagger();
+ //   app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Apply CORS Policy
 app.UseCors("AllowAllPolicy");
